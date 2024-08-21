78cacd0fcdd62fd98e386ce2bb3402d30480f0b1
filipfernandesphd <filipe.fernandes@ifsudestemg.edu.br>
2024-08-21 14:07:33 -0300

    feat: componente enemy atualizado para criar os inimigos dinamicamente

--- aframe-enemy.js
+++ aframe-enemy.js
@@ -1,21 +1,27 @@
 AFRAME.registerComponent('enemy',{
+    schema: {
+        amount: {'type': 'number', default: 3} //ex. no html enemy="amount:3"
+    },
+
     init: function() {
         this.create_enemies();   
     },
 
     create_enemies: function(){
-        let emeny = document.createElement('a-box');
-        emeny.classList.add('collidable');  
-        // emeny.setAttribute('dynamic-body','');
-        // enemy.setAttribute('static-body','');
-        emeny.setAttribute('random-position',{
-            min: `${this.getRandomNumber(-2, 0, 2)} ${this.getRandomNumber(.5, 1, 2)} ${this.getRandomNumber(0, 0, 2)}`,
-            max: `${this.getRandomNumber(0, 2, 2)} ${this.getRandomNumber(.5, 5, 2)} ${this.getRandomNumber(0, -5, 2)}`
-          }
-        );
-        emeny.setAttribute('random-color','');
+        for(let i=1; i<=this.data.amount; i++){
+            let emeny = document.createElement('a-box');
+            emeny.classList.add('collidable');  
+            // emeny.setAttribute('dynamic-body','');
+            // enemy.setAttribute('static-body','');
+            emeny.setAttribute('random-position',{
+                min: `${this.getRandomNumber(-2, 0, 2)} ${this.getRandomNumber(.5, 1, 2)} ${this.getRandomNumber(0, 0, 2)}`,
+                max: `${this.getRandomNumber(0, 2, 2)} ${this.getRandomNumber(.5, 5, 2)} ${this.getRandomNumber(0, -5, 2)}`
+            }
+            );
+            emeny.setAttribute('random-color','');
 
-        this.el.sceneEl.append(emeny);
+            this.el.sceneEl.append(emeny);
+        }
     },
 
     // função que retorna um número aleatório
