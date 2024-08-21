2aba5b7d340423a8d9c8ae8250f33ab91ebd73df
filipfernandesphd <filipe.fernandes@ifsudestemg.edu.br>
2024-08-21 18:42:10 -0300

    feat: componente scoreboard

---
+++ aframe-scoreboard.js
@@ -0,0 +1,43 @@
+// registering a component
+AFRAME.registerComponent('score-board', {
+    schema:{
+        position: {type:'vec3', default:'0 2.1 -3'}
+    },
+
+    init: function(){    
+        var self = this;
+        
+        this.createBoard();
+        this.createText();
+    },
+
+    createBoard: function(){
+        let board = document.createElement('a-plane');
+
+        board.setAttribute('color','black');
+        board.setAttribute('width','1.3');
+        board.setAttribute('height','.5');
+        board.setAttribute('position', this.data.position);
+            
+        this.el.appendChild(board);
+    },
+
+    createText: function(){
+        let position = {
+            x:this.data.position.x,
+            y:this.data.position.y - .055,
+            z:this.data.position.z + .1
+        };
+
+        let text = document.createElement('a-text');
+
+        text.setAttribute('id','textscore');
+        text.setAttribute('value',`0 / 3`);
+        text.setAttribute('width','8');
+        text.setAttribute('color','white');
+        text.setAttribute('align','center');
+        text.setAttribute('position',position);
+
+        this.el.appendChild(text);
+    }
+});
